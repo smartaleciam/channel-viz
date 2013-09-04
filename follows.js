137a98a97451b68dd6a9ce6af6c1020dc53dac51
smartaleciam <smartaleciam@yahoo.com>
2013-09-04 17:20:15 +0930

    Update follows.js

--- follows.js
+++ follows.js
@@ -15,8 +15,8 @@
 	var defaultKey		= 'FbgeUstX5sAvdxDOvaViBUHA35hQikirqK4ztUMwLDSkTJ84', // Unique master Xively API key to be used as a default
 		defaultFeeds	= [1410670764], // Comma separated array of Xively Feed ID numbers
 		applicationName	= 'Smartalec\'s Weather', // Replaces Xively logo in the header
-		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
-		dataInterval	= 30, // Default interval for data to be displayed (in seconds)
+		dataDuration	= '30', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
+		dataInterval	= '', // Default interval for data to be displayed (in seconds)
 		dataColor		= '6495ED', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 1; // To hide input form use value of 1, otherwise set to 0
 
